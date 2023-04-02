dd67b282e2e7bb9afb0eca2b9884a21bb7ed8c07
Raphael <raphael@dynamored.com>
2023-04-02 04:06:50 +0200

    docs(init): Add base documentation and configurations

---
+++ SECURITY.md
@@ -0,0 +1,20 @@
+# 🔐 Security Policy
+
+---
+
+## Supported Versions
+
+| Version      | Supported          |
+| ------------ | :----------------: |
+| `2.0.x`-stable | ✅ |
+| `1.x.x`-stable | ❌ |
+
+---
+
+## Reporting a Vulnerability
+
+Contact us via our discord support: https://umaestro.fr
+
+## Reporting a Bug
+
+Open a new issue using our bug report template
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,77 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    "@nuxtjs/tailwindcss",
-    "@nuxtjs/i18n",
-    "@pinia/nuxt",
-    "@nuxtjs/color-mode",
-  ],
+	app: {
+		head: {
+			title: `MakeBetter.app`,
+			htmlAttrs: {
+				lang: 'fr'
+			},
+			meta: [
+				{ charset: 'utf-8' },
 
-  devtools: {
-    enabled: true,
-  },
+				{ "http-equiv": 'description', name: 'description', content: `Site regroupant une multitude d'outils dans de multiples domaines différents.` },
+				{ hid: 'description', name: 'description', content: `Site regroupant une multitude d'outils dans de multiples domaines différents.` },
+
+				{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+				{ name: 'format-detection', content: 'telephone=no' },
+
+				{ name: 'og:type', content: "product" },
+				{ name: 'og:title', content: "MakeBetter.app - Créer ton meilleur projet avec nous !" },
+				{ name: 'og:description', content: "Site regroupant une multitude d'outils dans de multiples domaines différents." },
+				{ name: 'og:image', content: "https://makebetter.app/thumbnail.png" },
+
+				{ name: 'twitter:title', content: "MakeBetter.app - Créer ton meilleur projet avec nous !" },
+				{ name: 'twitter:description', content: "Site regroupant une multitude d'outils dans de multiples domaines différents." },
+				{ name: 'twitter:card', content: "summary_large_image" },
+				{ name: 'twitter:image', content: "https://makebetter.app/thumbnail.png" },
+
+				{ name: 'theme-color', content: "#004be0" },
+				{ name: 'msapplication-TileColor', content: "#004be0" },
+				{ name: 'msapplication-TileImage', content: "/ms-icon-144x144.png" },
+			],
+			link: [
+				{ rel: 'icon', type: 'image/png', sizes: '192x192', href: '/android-icon-192x192.png' },
+
+				{ rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+				{ rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+				{ rel: 'icon', type: 'image/png', sizes: "64x64", href: '/favicon-64x64.png' },
+
+				{ rel: 'apple-touch-icon', sizes: '57x57', href: '/apple-icon-57x57.png' },
+				{ rel: 'apple-touch-icon', sizes: '60x60', href: '/apple-icon-60x60.png' },
+				{ rel: 'apple-touch-icon', sizes: '72x72', href: '/apple-icon-72x72.png' },
+				{ rel: 'apple-touch-icon', sizes: '76x76', href: '/apple-icon-76x76.png' },
+				{ rel: 'apple-touch-icon', sizes: '114x114', href: '/apple-icon-114x114.png' },
+				{ rel: 'apple-touch-icon', sizes: '120x120', href: '/apple-icon-120x120.png' },
+				{ rel: 'apple-touch-icon', sizes: '144x144', href: '/apple-icon-144x144.png' },
+				{ rel: 'apple-touch-icon', sizes: '152x152', href: '/apple-icon-152x152.png' },
+				{ rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-icon-180x180.png' },
+
+				{ rel: 'manifest', href: '/manifest.json' },
+			]
+		}
+	},
+
+	modules: [
+		"@nuxtjs/tailwindcss",
+		"@nuxtjs/i18n",
+		"@pinia/nuxt",
+		"@nuxtjs/color-mode",
+		'@nuxt/content',
+	],
+
+	devtools: {
+		enabled: true,
+	},
+
+	robots: {
+		// https://nuxt.com/modules/robots
+	},
+
+	content: {
+		// https://content.nuxtjs.org/api/configuration
+	},
+
+	typescript: {
+		typeCheck: true,
+	},
 });
