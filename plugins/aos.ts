25689eccd3d4d3a3f798a4310172cb92040bb451
BriqueAuBob <briqueaubob@gmail.com>
2023-08-02 01:36:19 +0200

    test

--- plugins/aos.ts
+++ plugins/aos.ts
@@ -2,6 +2,7 @@ import { defineNuxtPlugin } from 'nuxt/app';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
+// aos plugin
 export default defineNuxtPlugin((nuxtApp) => {
     if (typeof window === 'undefined') return;
     nuxtApp.AOS = AOS.init({
