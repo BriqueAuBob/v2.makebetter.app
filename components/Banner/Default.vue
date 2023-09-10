f2b62c2e29e0e5a4e508f48abb417e37d21e3870
Brique Au Bob <briqueaubob@gmail.com>
2023-09-10 22:31:50 +0200

    :lipstick: New recruitment banner

--- components/Banner/Default.vue
+++ components/Banner/Default.vue
@@ -20,10 +20,16 @@ defineProps({
         class="bg-primary-500 bg-cover bg-center bg-no-repeat py-16"
         :style="`background-image: url(${image})`"
     >
+        <slot name="header"></slot>
         <div class="container grid items-center gap-8 lg:grid-cols-4">
-            <div class="col-span-3">
+            <div :class="$slots.right ? 'col-span-3' : 'col-span-4'">
                 <h1 class="text-4xl font-bold text-white">{{ title }}</h1>
-                <p class="mt-2 text-lg text-white">{{ description }}</p>
+                <p
+                    class="mt-2 text-lg text-white"
+                    :class="!$slots.right && 'mx-auto max-w-2xl'"
+                >
+                    {{ description }}
+                </p>
                 <slot name="left"></slot>
             </div>
             <div class="hidden lg:block">
