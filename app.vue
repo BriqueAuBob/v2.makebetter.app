1ef45fb3f59d35b36bc144db72812075d233f597
BriqueAuBob <briqueaubob@gmail.com>
2023-10-17 22:56:05 +0200

    :lipstick: Halloween mode

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { $fetchApi } from '~/composables/fetch';
 import { useAuthStore } from '~/stores/auth';
+import { useDebounce } from '~/composables/debounce';
 
 const authStore = useAuthStore();
 const router = useRouter();
@@ -32,12 +33,45 @@ useHead({
         lang: i18nHead.value.htmlAttrs!.lang,
     },
 });
+
+const mouseX = ref(0);
+const mouseY = ref(0);
+const ghostStyle = computed(() => {
+    if (!process.client) return;
+    const x = Math.floor((Math.random() - 0.5) * 250);
+    const y = Math.floor((Math.random() - 0.5) * 250);
+
+    return {
+        left: `${mouseX.value + x}px`,
+        top: `${mouseY.value + y}px`,
+    };
+});
+
+const setMousePosition = useDebounce(50, (e: any) => {
+    mouseX.value = e.clientX + window.scrollX;
+    mouseY.value = e.clientY + window.scrollY;
+});
 </script>
 
 <template>
-    <div class="antialiased">
+    <div
+        class="antialiased"
+        @mousemove="
+            (e) => {
+                setMousePosition(e);
+            }
+        "
+    >
         <NuxtLayout>
             <NuxtPage />
         </NuxtLayout>
+        <ClientOnly>
+            <img
+                src="/images/events/halloween/2023/ghost.png"
+                alt="ghost"
+                class="ghost pointer-events-none hidden lg:inline"
+                :style="ghostStyle"
+            />
+        </ClientOnly>
     </div>
 </template>
