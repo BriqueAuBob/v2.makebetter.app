f965e305c590ff09b4147c1bb8a2e77cfd7e231b
BriqueAuBob <briqueaubob@gmail.com>
2024-04-13 18:22:07 +0200

    :construction: code related to the new sockets implementation

--- composables/socket.ts
+++ composables/socket.ts
@@ -10,7 +10,7 @@ const useSocket = (sessionId: string) => {
     const cookie = useCookie('token');
     const authStore = useAuthStore();
 
-    const socket = io('https://api.umaestro.fr', {
+    const socket = io('http://localhost:3333/', {
         transports: ['websocket'],
         query: {
             sessionId,
--- pages/tools/discord/embed-maker.vue
+++ pages/tools/discord/embed-maker.vue
@@ -78,18 +78,19 @@ const onLeavePage = () => {
 onMounted(async () => {
     if (route.query.id) {
         try {
-            const { save } = await $fetchApi<{ save: any }>(`/makebetter/tools/saves/${route.query.id}`);
-            if (save?.permissions?.length > 0) {
-                socket.value = useSocket(route.query.id as string);
-            }
-            embedMakerStore.editingSave = save;
-            embedMakerStore.messages = save.data;
+            // const { save } = await $fetchApi<{ save: any }>(`/makebetter/tools/saves/${route.query.id}`);
+            // if (save?.permissions?.length > 0) {
+            socket.value = useSocket(route.query.id as string);
+            // }
+            // embedMakerStore.editingSave = save;
+            // embedMakerStore.messages = save.data;
         } catch (err) {
-            await router.push({
-                query: {
-                    id: undefined,
-                },
-            });
+            console.log(err);
+            // await router.push({
+            //     query: {
+            //         id: undefined,
+            //     },
+            // });
         }
     }
     window.onunload = onLeavePage;
